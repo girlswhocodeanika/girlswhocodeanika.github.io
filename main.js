5d20f3a6b730edfc2d6a0abcf5bfe938591956cb
girlswhocodeanika <33737002+girlswhocodeanika@users.noreply.github.com>
2017-11-16 16:12:33 -0800

    updated phrases

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function math(){
 var answer = prompt("Enter 1+1");
 if (answer == 2) {
- confirm("You got it right. Your answer was " + answer);
+ confirm("You got it correct. Your answer was " + answer);
 }
 else {
- confirm("You got it wrong");
+ confirm("Nope, thats completly wrong!");
  }
 }
